e2d808f469563d996fd50ea0d71f3527d0d06ffc
higaredajra <higaredajra@msoe.edu>
2025-10-15 14:32:43 -0500

    Implement predict_loop()

--- knn.py
+++ knn.py
@@ -1,4 +1,5 @@
 import random
+from operator import indexOf
 
 import numpy as np
 from scipy.spatial import distance
@@ -28,4 +29,30 @@ class KNN:
         Predicts the output variable's values for the query points X using loops.
         
         """
+        solved_types = []
+        for j in range (len(X)):
+            distances = []
+            y_types = []
+            y_types_hits = []
+            for i in range (len(self.X)): # get the point's distance from each know point
+                # add the distance and classification type as a tuple to the distances list
+                distances.append((distance.euclidean(self.X[i], X[j]), self.y[i]))
+                # add only new types to the y_types list
+                if self.y[i] not in y_types:
+                    y_types.append(self.y[i])
+                    y_types_hits.append(0) # set the number of hits variable for each y_type to 0
+            distances = sorted(distances, key=lambda dist: dist[0]) # sort distances from least to greatest
 
+            for i in range (self.k): # look at each of the closest neighbors and note the y_type
+                y_types_hits[indexOf(y_types, distances[i][1])] += 1
+
+            most_likely_type = y_types[0]
+            max_hits = y_types_hits[0]
+            for i in range (len(y_types_hits)):
+                if y_types_hits[i] > max_hits:
+                    most_likely_type = y_types[i]
+                    max_hits = y_types_hits[i]
+
+            solved_types.append(most_likely_type)
+
+        return np.asarray(solved_types)
